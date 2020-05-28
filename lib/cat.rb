f3499b304c8207bbc0f7789cbd2678ef6858ad93
Jonkavaliauskas <jonas.kavaliauskas@yale.edu>
2020-05-29 01:02:49 +0300

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,27 @@
 class Cat
   # code goes here
+  attr_accessor :mood, :owner
+  attr_reader :name
+  @@all = []
+
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+  
+  def cats
+    Cat.all.select{|cat| cat.owner == self}
+  end
+  
+  def dogs
+    Dog.all.select{|dog| dog.owner == self}
+  end  
+
+
 end
